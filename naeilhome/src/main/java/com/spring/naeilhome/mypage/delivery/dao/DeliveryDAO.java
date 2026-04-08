4535ced5a71d7ae452882ce8fa27fd7ef3783d0f
Lim HyeonKyu <lhk858585@gmail.com>
2026-04-08 22:16:46 +0900

    Update DeliveryDAO.java

--- naeilhome/src/main/java/com/spring/naeilhome/mypage/delivery/dao/DeliveryDAO.java
+++ naeilhome/src/main/java/com/spring/naeilhome/mypage/delivery/dao/DeliveryDAO.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import org.springframework.dao.DataAccessException;
 
 import com.spring.naeilhome.mypage.delivery.domain.DeliveryDomain;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface DeliveryDAO {
 
 	public List<DeliveryDomain> ordersList(String memberId) throws DataAccessException;
