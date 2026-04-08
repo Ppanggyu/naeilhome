82093ed95894866bbc21895ce0a09de0714e174a
Lim HyeonKyu <lhk858585@gmail.com>
2026-04-08 22:15:20 +0900

    Update CreDAO.java

--- naeilhome/src/main/java/com/spring/naeilhome/cre/dao/CreDAO.java
+++ naeilhome/src/main/java/com/spring/naeilhome/cre/dao/CreDAO.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import org.springframework.dao.DataAccessException;
 
 import com.spring.naeilhome.mypage.delivery.domain.DeliveryDomain;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface CreDAO {
 	
 	public DeliveryDomain creProductApplication(Map<String, Object> map) throws DataAccessException;
