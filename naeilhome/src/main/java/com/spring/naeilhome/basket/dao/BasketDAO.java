26791cb47df83454b125e02561e2291dd4336a23
Lim HyeonKyu <lhk858585@gmail.com>
2026-04-08 22:13:59 +0900

    Update BasketDAO.java

--- naeilhome/src/main/java/com/spring/naeilhome/basket/dao/BasketDAO.java
+++ naeilhome/src/main/java/com/spring/naeilhome/basket/dao/BasketDAO.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import com.spring.naeilhome.basket.domain.BasketDomain;
 import com.spring.naeilhome.basket.vo.BasketVO;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface BasketDAO {
 	
 	public List<BasketDomain> myBasketList(String memberId) throws Exception;
