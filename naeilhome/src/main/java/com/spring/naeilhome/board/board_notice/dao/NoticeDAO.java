7ffb4d497f08b63081ce312ddc30ebf54550f78c
Lim HyeonKyu <lhk858585@gmail.com>
2026-04-08 22:14:38 +0900

    Update NoticeDAO.java

--- naeilhome/src/main/java/com/spring/naeilhome/board/board_notice/dao/NoticeDAO.java
+++ naeilhome/src/main/java/com/spring/naeilhome/board/board_notice/dao/NoticeDAO.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.dao.DataAccessException;
 
 import com.spring.naeilhome.board.board_notice.domain.NoticeDomain;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface NoticeDAO {
 	
 	public List<NoticeDomain> noticeList() throws DataAccessException;
