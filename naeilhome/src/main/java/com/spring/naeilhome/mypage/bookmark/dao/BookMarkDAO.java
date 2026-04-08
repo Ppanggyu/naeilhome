0a71eeabd119649c03b6a117b5956ca9069be433
Lim HyeonKyu <lhk858585@gmail.com>
2026-04-08 22:54:35 +0900

    Update BookMarkDAO.java

--- naeilhome/src/main/java/com/spring/naeilhome/mypage/bookmark/dao/BookMarkDAO.java
+++ naeilhome/src/main/java/com/spring/naeilhome/mypage/bookmark/dao/BookMarkDAO.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.spring.naeilhome.board.board_myhome.domain.MyhomeDomain;
 
-@Repository("bookMarkDAO")
+
 public interface BookMarkDAO {
 
 	public List<MyhomeDomain> selectBookMarkList(Map<String, Object> myhomeMap) throws DataAccessException;
