f632c5d77b1025040b5221274b653ba43bab6542
Lim HyeonKyu <lhk858585@gmail.com>
2026-04-08 22:53:04 +0900

    Update KakaoDAO.java

--- naeilhome/src/main/java/com/spring/naeilhome/social/dao/KakaoDAO.java
+++ naeilhome/src/main/java/com/spring/naeilhome/social/dao/KakaoDAO.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spring.naeilhome.member.domain.MemberDomain;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 @Repository("kakaoDAO")
 public class KakaoDAO {
 	
